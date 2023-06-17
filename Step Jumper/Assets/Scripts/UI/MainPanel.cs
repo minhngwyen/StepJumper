7c21b7a4e406489559b7d88a0f0cefb9ad8d4015
minhpro279 <minhpro279@gmail.com>
2023-06-18 02:26:40 +0700

    Button Update
    + Added Option button to main menu
    + Added script to change panel for High Score button and Option button
    + Fix button not working on click
    + Added new GameScene for Start Button

--- Step Jumper/Assets/Scripts/UI/MainPanel.cs
+++ Step Jumper/Assets/Scripts/UI/MainPanel.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainPanel : MonoBehaviour
@@ -9,31 +10,26 @@ public class MainPanel : MonoBehaviour
     [SerializeField]
     private Button buttonStart;
     [SerializeField]
-    private Button buttonHighScore;
-    [SerializeField]
     private Button buttonExit;
+
+    public int gameSceneNum;
     // Start is called before the first frame update
     void Start()
     {
         buttonStart.onClick.AddListener(OnButtonStartClick);
-        buttonHighScore.onClick.AddListener(OnButtonHighScoreButton);
         buttonExit.onClick.AddListener(OnButtonExitButton);
 
     }
 
     private void OnButtonExitButton()
     {
+        Debug.Log("QUIT!");
         Application.Quit();
-    }
-
-    private void OnButtonHighScoreButton()
-    {
-        
     }
 
     private void OnButtonStartClick()
     {
-        
+        SceneManager.LoadScene(gameSceneNum);
     }
 
     // Update is called once per frame
