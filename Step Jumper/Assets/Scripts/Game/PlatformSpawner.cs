6991898a49e35681a00238b1be26ed586a4affa1
ngoctb13 <ngoctb136@gmail.com>
2023-06-26 02:47:14 +0700

    update PlatformSpawner and add some prefabs

--- Step Jumper/Assets/Scripts/Game/PlatformSpawner.cs
+++ Step Jumper/Assets/Scripts/Game/PlatformSpawner.cs
@@ -33,7 +33,16 @@ public class PlatformSpawner : MonoBehaviour
     /// <summary>
     /// </summary>
     private PlatformGroupType groupType;
-
+    /// <summary>
+    /// </summary>
+    private bool spikeSpawnLeft = false;
+    /// <summary>
+    /// </summary>
+    private Vector3 spikeDirPlatformPos;
+    /// <summary>
+    /// </summary>
+    private int afterSpawnSpikeSpawnCount;
+    private bool isSpawnSpike;
 
     private void Awake()
     {
@@ -68,12 +77,15 @@ public class PlatformSpawner : MonoBehaviour
     /// </summary>
     private void UpdateFallTime()
     {
+        if (GameManager.Instance.GetGameScore() > milestoneCount)
+        {
             milestoneCount *= 2;
             fallTime *= multiple;
             if (fallTime < minFallTime)
             {
                 fallTime = minFallTime;
             }
+        }
     }
     /// <summary>
     /// </summary>
@@ -95,11 +107,11 @@ public class PlatformSpawner : MonoBehaviour
     /// </summary>
     private void DecidePath()
     {
-        //if (isSpawnSpike)
-        //{
-        //    AfterSpawnSpike();
-        //    return;
-        //}
+        if (isSpawnSpike)
+        {
+            AfterSpawnSpike();
+            return;
+        }
         if (spawnPlatformCount > 0)
         {
             spawnPlatformCount--;
@@ -124,47 +136,61 @@ public class PlatformSpawner : MonoBehaviour
         }
         else if (spawnPlatformCount == 0)
         {
-            //int ran = Random.Range(0, 3);
-            //if (ran == 0)
-            //{
+            int ran = Random.Range(0, 3);
+            if (ran == 0)
+            {
                 SpawnCommonPlatformGroup(ranObstacleDir);
-            //}
-            //else if (ran == 1)
-            //{
-            //    switch (groupType)
-            //    {
-            //        case PlatformGroupType.Grass:
-            //            SpawnGrassPlatformGroup(ranObstacleDir);
-            //            break;
-            //        case PlatformGroupType.Winter:
-            //            SpawnWinterPlatformGroup(ranObstacleDir);
-            //            break;
-            //        default:
-            //            break;
-            //    }
-            //}
-            //else
-            //{
-            //    int value = -1;
-            //    if (isLeftSpawn)
-            //    {
-            //        value = 0;
-            //    }
-            //    else
-            //    {
-            //        value = 1;
-            //    }
-            //}
+            }
+            else if (ran == 1)
+            {
+                switch (groupType)
+                {
+                    case PlatformGroupType.Grass:
+                        SpawnGrassPlatformGroup(ranObstacleDir);
+                        break;
+                    case PlatformGroupType.Winter:
+                        SpawnWinterPlatformGroup(ranObstacleDir);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                int value = -1;
+                if (isLeftSpawn)
+                {
+                    value = 0;
+                }
+                else
+                {
+                    value = 1;
+                }
+                SpawnSpikePlatform(value);
+
+                isSpawnSpike = true;
+                afterSpawnSpikeSpawnCount = 4;
+                if (spikeSpawnLeft)
+                {
+                    spikeDirPlatformPos = new Vector3(platformSpawnPosition.x - 1.65f,
+                        platformSpawnPosition.y + vars.nextYPos, 0);
+                }
+                else
+                {
+                    spikeDirPlatformPos = new Vector3(platformSpawnPosition.x + 1.65f,
+                        platformSpawnPosition.y + vars.nextYPos, 0);
+                }
+            }
         }
 
-        //int ranSpawnDiamond = Random.Range(0, 8);
-        //if (ranSpawnDiamond >= 6 && GameManager.Instance.PlayerIsMove)
-        //{
-        //    GameObject go = ObjectPool.Instance.GetDiamond();
-        //    go.transform.position = new Vector3(platformSpawnPosition.x,
-        //        platformSpawnPosition.y + 0.5f, 0);
-        //    go.SetActive(true);
-        //}
+        int ranSpawnDiamond = Random.Range(0, 8);
+        if (ranSpawnDiamond >= 6 && GameManager.Instance.PlayerIsMove)
+        {
+            GameObject go = ObjectPool.Instance.GetDiamond();
+            go.transform.position = new Vector3(platformSpawnPosition.x,
+                platformSpawnPosition.y + 0.5f, 0);
+            go.SetActive(true);
+        }
         if (isLeftSpawn)
         {
             platformSpawnPosition = new Vector3(platformSpawnPosition.x - vars.nextXPos,
@@ -198,20 +224,89 @@ public class PlatformSpawner : MonoBehaviour
     /// </summary>
     private void SpawnGrassPlatformGroup(int ranObstacleDir)
     {
-        //GameObject go = ObjectPool.Instance.GetGrassPlatformGroup();
-        //go.transform.position = platformSpawnPosition;
-        //go.GetComponent<PlatformScript>().Init(selectPlatformSprite, fallTime, ranObstacleDir);
-        //go.SetActive(true);
+        GameObject go = ObjectPool.Instance.GetGrassPlatformGroup();
+        go.transform.position = platformSpawnPosition;
+        go.GetComponent<PlatformScript>().Init(selectPlatformSprite, fallTime, ranObstacleDir);
+        go.SetActive(true);
     }
     /// <summary>
     /// </summary>
     private void SpawnWinterPlatformGroup(int ranObstacleDir)
     {
-        //GameObject go = ObjectPool.Instance.GetWinterPlatformGroup();
-        //go.transform.position = platformSpawnPosition;
-        //go.GetComponent<PlatformScript>().Init(selectPlatformSprite, fallTime, ranObstacleDir);
-        //go.SetActive(true);
+        GameObject go = ObjectPool.Instance.GetWinterPlatformGroup();
+        go.transform.position = platformSpawnPosition;
+        go.GetComponent<PlatformScript>().Init(selectPlatformSprite, fallTime, ranObstacleDir);
+        go.SetActive(true);
     }
-   
-    
+    /// <summary>
+    /// </summary>
+    /// <param name="dir"></param>
+    private void SpawnSpikePlatform(int dir)
+    {
+        GameObject temp = null;
+        if (dir == 0)
+        {
+            spikeSpawnLeft = false;
+            temp = ObjectPool.Instance.GetRightSpikePlatform();
+        }
+        else
+        {
+            spikeSpawnLeft = true;
+            temp = ObjectPool.Instance.GetLeftSpikePlatform();
+        }
+        temp.transform.position = platformSpawnPosition;
+        temp.GetComponent<PlatformScript>().Init(selectPlatformSprite, fallTime, dir);
+        temp.SetActive(true);
+    }
+    /// <summary>
+
+    /// </summary>
+    private void AfterSpawnSpike()
+    {
+        if (afterSpawnSpikeSpawnCount > 0)
+        {
+            afterSpawnSpikeSpawnCount--;
+            for (int i = 0; i < 2; i++)
+            {
+                GameObject temp = ObjectPool.Instance.GetNormalPlatform();
+                if (i == 0)
+                {
+                    temp.transform.position = platformSpawnPosition;
+
+                    if (spikeSpawnLeft)
+                    {
+                        platformSpawnPosition = new Vector3(platformSpawnPosition.x + vars.nextXPos,
+                            platformSpawnPosition.y + vars.nextYPos, 0);
+                    }
+                    else
+                    {
+                        platformSpawnPosition = new Vector3(platformSpawnPosition.x - vars.nextXPos,
+                            platformSpawnPosition.y + vars.nextYPos, 0);
+                    }
+                }
+                else
+                {
+                    temp.transform.position = spikeDirPlatformPos;
+                    if (spikeSpawnLeft)
+                    {
+                        spikeDirPlatformPos = new Vector3(spikeDirPlatformPos.x - vars.nextXPos,
+                            spikeDirPlatformPos.y + vars.nextYPos, 0);
+                    }
+                    else
+                    {
+                        spikeDirPlatformPos = new Vector3(spikeDirPlatformPos.x + vars.nextXPos,
+                            spikeDirPlatformPos.y + vars.nextYPos, 0);
+                    }
+                }
+
+                temp.GetComponent<PlatformScript>().Init(selectPlatformSprite, fallTime, 1);
+                temp.SetActive(true);
+            }
+        }
+        else
+        {
+            isSpawnSpike = false;
+            DecidePath();
+        }
+    }
 }
